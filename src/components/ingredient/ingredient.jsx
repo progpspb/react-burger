46159184c11c56178f2f3f74e13662d1675081b6
Alex <progps@inbox.ru>
2024-02-28 14:52:36 +0300

    change ingredient component
    - useState
    + useModal

--- src/components/ingredient/ingredient.jsx
+++ src/components/ingredient/ingredient.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react';
 import styles from './ingredient.module.css';
 import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import IngredientDetails from '../ingredient-details/ingredient-details';
 import Modal from '../modal/modal';
+import useModal from '../../hooks/useModal.js';
 import ingredientPropTypes from '../../prop-types/ingredient.types.jsx';
 
 const Ingredient = ({ ingredient }) => {
 
-    const [showModal, setShowModal] = useState(false);
-
-    function getModal() {
-        setShowModal(!showModal);
-    }
-
+    const { isModalOpen, openModal, closeModal } = useModal(false);
+    
     return (
         <>
-        <div className={styles.item} onClick = { getModal }>
+        <div className={styles.item} onClick = { openModal }>
             {ingredient._id === 0 && <Counter count={1} size="default" /> }
             <img src={ingredient.image_large} alt={ingredient.name} />
             <div className={styles.price}>
@@ -24,11 +20,11 @@ const Ingredient = ({ ingredient }) => {
             </div>
             <div className='mt-2 mb-6'>{ingredient.name}</div>
         </div>        
-        {showModal && ( 
-            <Modal title="Детали ингредиента"  onClose = { getModal }>
+        {isModalOpen && ( 
+            <Modal title="Детали ингредиента"  onClose = { closeModal }>
                 <IngredientDetails ingredient = { ingredient } />
             </Modal>
-        )}     
+        )}
         </>
     )
 }
